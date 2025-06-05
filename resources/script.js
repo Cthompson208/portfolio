aef2b88b15737a1ee1908a77a7252c93e5c4857a
Chey T. <204792064+Cthompson208@users.noreply.github.com>
2025-06-04 17:05:21 -0700

    tweaking js game code not finished yet though

--- resources/script.js
+++ resources/script.js
@@ -11,6 +11,7 @@ let tieText = document.getElementById('tie');
 let bombText = document.getElementById('blown_up');
 let counterNum = document.getElementById('counter_num');
 let userValue = document.getElementById('choosing');
+let c = 0
 
 
 const getUserChoice = userInput => {
@@ -40,55 +41,135 @@ const getComputerChoice = () => {
 const determineWinner = (userChoice, computerChoice) => {
   if (userChoice === 'bomb') {
     userBombImg.style.visibility = 'visible';
-    bombText.style.visibility = 'visible';
+    userPaperImg.style.visibility = 'hidden';
+    userRockImg.style.visibility = 'hidden';
+    userScissorsImg.style.visibility = 'hidden';
     if (computerChoice === 'rock') {
+        c++;
         computerRockImg.style.visibility = 'visible';
+        computerPaperImg.style.visibility = 'hidden';
+        computerScissorsImg.style.visibility = 'hidden';
+        winTextstyle.visibility = 'hidden';
+        loseText.style.visibility = 'hidden';
+        tieText.style.visibility = 'hidden';
+        bombText.style.visibility = 'visible';
     } else if (computerChoice === 'paper') {
+        c++;
         computerPaperImg.style.visibility = 'visible';
+        computerRockImg.style.visibility = 'hidden';
+        computerScissorsImg.style.visibility = 'hidden';
+        winTextstyle.visibility = 'hidden';
+        loseText.style.visibility = 'hidden';
+        tieText.style.visibility = 'hidden';
+        bombText.style.visibility = 'visible';
     } else if (computerChoice === 'scissors') {
+        c++;
         computerScissorsImg.style.visibility = 'visible';
+        computerPaperImg.style.visibility = 'hidden';
+        computerRockImg.style.visibility = 'hidden';
+        winTextstyle.visibility = 'hidden';
+        loseText.style.visibility = 'hidden';
+        tieText.style.visibility = 'hidden';
+        bombText.style.visibility = 'visible';
     }
   }
 
   if (userChoice === 'rock') {
     userRockImg.style.visibility = 'visible';
+    userBombImg.style.visibility = 'hidden';
+    userPaperImg.style.visibility = 'hidden';
+    userScissorsImg.style.visibility = 'hidden';
     if (computerChoice === 'paper') {
       computerPaperImg.style.visibility = 'visible';
+      computerRockImg.style.visibility = 'hidden';
+      computerScissorsImg.style.visibility = 'hidden';
       loseText.style.visibility = 'visible';
+      winText.style.visibility = 'hidden';
+      tieText.style.visibility = 'hidden';
+      bombText.style.visibility = 'hidden';
     } else if (computerChoice === 'rock') {
         computerRockImg.style.visibility = 'visible';
+        computerPaperImg.style.visibility = 'hidden';
+        computerScissorsImg.style.visibility = 'hidden';
         tieText.style.visibility = 'visible';
+        winText.style.visibility = 'hidden';
+        loseText.style.visibility = 'hidden';
+        bombText.style.visibility = 'hidden';
     } else if (computerChoice === 'scissors') {
         computerScissorsImg.style.visibility = 'visible';
+        computerPaperImg.style.visibility = 'hidden';
+        computerRockImg.style.visibility = 'hidden';
         winText.style.visibility = 'visible';
+        loseText.style.visibility = 'hidden';
+        tieText.style.visibility = 'hidden';
+        bombText.style.visibility = 'hidden';
+        c++;
     }
   }
 
   if (userChoice === 'paper') {
     userPaperImg.style.visibility = 'visible';
+    userRockImg.style.visibility = 'hidden';
+    userScissorsImg.style.visibility = 'hidden';
+    userBombImg.style.visibility = 'hidden';
     if (computerChoice === 'scissors') {
       computerScissorsImg.style.visibility = 'visible';
+      computerPaperImg.style.visibility = 'hidden';
+      computerRockImg.style.visibility = 'hidden';
       loseText.style.visibility = 'visible';
+      winText.style.visibility = 'hidden';
+      tieText.style.visibility = 'hidden';
+      bombText.style.visibility = 'hidden';
     } else if (computerChoice === 'paper') {
         computerPaperImg.style.visibility = 'visible';
+        computerRockImg.style.visibility = 'hidden';
+        computerScissorsImg.style.visibility = 'hidden';
         tieText.style.visibility = 'visible';
+        winText.style.visibility = 'hidden';
+        loseText.style.visibility = 'hidden';
+        bombText.style.visibility = 'hidden';
     } else if (computerChoice === 'rock') {
         computerRockImg.style.visibility = 'visible';
+        computerPaperImg.style.visibility = 'hidden';
+        computerScissorsImg.style.visibility = 'hidden';
         winText.style.visibility = 'visible';
+        loseText.style.visibility = 'hidden';
+        tieText.style.visibility = 'hidden';
+        bombText.style.visibility = 'hidden';
+        c++;
     }
   }
 
   if (userChoice === 'scissors') {
     userScissorsImg.style.visibility = 'visible';
+    userBombImg.style.visibility = 'hidden';
+    userPaperImg.style.visibility = 'hidden';
+    userRockImg.style.visibility = 'hidden';
     if (computerChoice === 'rock') {
       computerRockImg.style.visibility = 'visible';
+      computerPaperImg.style.visibility = 'hidden';
+      computerScissorsImg.style.visibility = 'hidden';
       loseText.style.visibility = 'visible';
+      winText.style.visibility = 'hidden';
+      tieText.style.visibility = 'hidden';
+      bombText.style.visibility = 'hidden';
     } else if (computerChoice === 'scissors') {
         computerScissorsImg.style.visibility = 'visible';
+        computerPaperImg.style.visibility = 'hidden';
+        computerRockImg.style.visibility = 'hidden';
         tieText.style.visibility = 'visible';
+        winText.style.visibility = 'hidden';
+        loseText.style.visibility = 'hidden';
+        bombText.style.visibility = 'hidden';
     } else if (computerChoice === 'paper') {
         computerPaperImg.style.visibility = 'visible';
+        computerRockImg.style.visibility = 'hidden';
+        computerScissorsImg.style.visibility = 'hidden';
         winText.style.visibility = 'visible';
+        loseText.style.visibility = 'hidden';
+        tieText.style.visibility = 'hidden';
+        bombText.style.visibility = 'hidden';
+        c++;
     }
   }
 };
@@ -97,6 +178,7 @@ const playGame = () => {
   let userChoice = getUserChoice(userValue.value);
   let computerChoice = getComputerChoice();
   determineWinner(userChoice, computerChoice);
+  counterNum.innerHTML = c;
 }
 
 let button = document.getElementById('submit');
